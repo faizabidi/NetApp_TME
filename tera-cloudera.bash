d15306a6142554fa8b04fb31ea07aea96ac592f2
faizabidi <fabidi89@vt.edu>
2018-03-01 16:19:46 -0500

    Gen, sort, and validate for cloudera.

--- tera-cloudera.bash
+++ tera-cloudera.bash
@@ -9,34 +9,53 @@ hadoop fs -rm -r  /user/faiz89/terasort-input
 sleep 1m
 # RUN 2
 # Remove the old data first
-hadoop fs -rm -r  /HiBench/Dfsioe/terasort-input
+hadoop fs -rm -r  /user/faiz89/terasort-input
 # Run the job
-/usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -D mapreduce.job.maps=304 -D mapreduce.job.reduces=304 10995116277 /HiBench/Dfsioe/terasort-input
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -D mapreduce.job.maps=304 -D mapreduce.job.reduces=304 10995116277 /user/faiz89/terasort-input
 sleep 1m
 # RUN 3
 # Remove the old data first
-hadoop fs -rm -r  /HiBench/Dfsioe/terasort-input
+hadoop fs -rm -r  /user/faiz89/terasort-input
 # Run the job
-/usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -D mapreduce.job.maps=304 -D mapreduce.job.reduces=304 10995116277 /HiBench/Dfsioe/terasort-input
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -D mapreduce.job.maps=304 -D mapreduce.job.reduces=304 10995116277 /user/faiz89/terasort-input
 sleep 1m
 #############################################################################################
 # Terasort
 # RUN 1
 # Remove the old output
-hadoop fs -rm -r /HiBench/Dfsioe/terasort-output
+hadoop fs -rm -r /user/faiz89/terasort-output
 # Run the job
-/usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /HiBench/Dfsioe/terasort-input /HiBench/Dfsioe/terasort-output
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /user/faiz89/terasort-input /user/faiz89/terasort-output
 sleep 1m
 # RUN 2
 # Remove the old output
-hadoop fs -rm -r /HiBench/Dfsioe/terasort-output
+hadoop fs -rm -r /user/faiz89/terasort-output
 # Run the job
-/usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /HiBench/Dfsioe/terasort-input /HiBench/Dfsioe/terasort-output
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /user/faiz89/terasort-input /user/faiz89/terasort-output
 sleep 1m
 # RUN 3
 # Remove the old output
-hadoop fs -rm -r /HiBench/Dfsioe/terasort-output
+hadoop fs -rm -r /user/faiz89/terasort-output
 # Run the job
-/usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /HiBench/Dfsioe/terasort-input /HiBench/Dfsioe/terasort-output
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /user/faiz89/terasort-input /user/faiz89/terasort-output
 sleep 1m
 #############################################################################################
+# Teravalidate
+# RUN 1
+# Remove the old output
+hadoop fs -rm -r /user/faiz89/terasort-validate-output
+# Run the job
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teravalidate -D mapreduce.job.reduces=304 /user/faiz89/terasort-output /user/faiz89/terasort-validate-output
+sleep 1m
+# RUN 2
+# Remove the old output
+hadoop fs -rm -r /user/faiz89/terasort-output
+# Run the job
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teravalidate -D mapreduce.job.reduces=304 /user/faiz89/terasort-output /user/faiz89/terasort-validate-output
+sleep 1m
+# RUN 3
+# Remove the old output
+hadoop fs -rm -r /user/faiz89/terasort-validate-output
+# Run the job
+/usr/bin/hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teravalidate -D mapreduce.job.reduces=304 /user/faiz89/terasort-output /user/faiz89/terasort-validate-output
+
