ada6b6f6c8c47dea40bbb16cc8ef350a67013c12
faizabidi <fabidi89@vt.edu>
2018-03-05 11:31:48 -0500

    DFSIO tests with output directory.

--- cloudera/dfsio-cloudera.bash
+++ cloudera/dfsio-cloudera.bash
@@ -3,7 +3,9 @@
 # DFSIO WRITE
 # RUN 1
 # Remove the old data
-hadoop jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -write -nrFiles 128 -fileSize 8192
+hadoop fs -rm -r /user/faiz89/benchmark/TestDFSIOwrite
+# Run the job
+hadoop jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -write -nrFiles 128 -fileSize 8192 -resFile /user/faiz89/benchmark/TestDFSIOwrite
 sleep 1m
 # RUN 2
 #hadoop jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -write -nrFiles 128 -fileSize 8192
@@ -14,7 +16,10 @@ sleep 1m
 #############################################################################################
 # DFSIO READ
 # RUN 1
-hadoop jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar -read -nrFiles 128 -fileSize 8192
+# Remove the old data fairt
+hadoop fs -rm -r /user/faiz89/benchmark/TestDFSIO
+# Run the job
+hadoop jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar -read -nrFiles 128 -fileSize 8192 -resFile /user/faiz89/benchmark/TestDFSIO
 #sleep 1m
 # RUN 2
 #hadoop jar /opt/cloudera/parcels/CDH-5.13.1-1.cdh5.13.1.p0.2/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar -read -nrFiles 128 -fileSize 8192
