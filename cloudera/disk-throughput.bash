e07a47539ae40c7d68d6335923650b27b0e49ceb
faizabidi <fabidi89@vt.edu>
2018-03-15 11:01:11 -0400

    Added back ther read test.

--- cloudera/disk-throughput.bash
+++ cloudera/disk-throughput.bash
@@ -11,13 +11,12 @@ chmod +x diskbench.pl
     --vector=writeSpeed --dir=/mnt/disk1 \
     --logfile=./diskbench-write.log 
 
-#sleep 30
+sleep 30
 
 # Run read test
-#/home/faiz89/git/NetApp_TME/cloudera/diskbench.pl --bs=64K --count=5000000 \
-#    --vector=readSpeed --dir=/mnt/disk1 \
-#    --logfile=./diskbench-read.log
-
+/home/faiz89/git/NetApp_TME/cloudera/diskbench.pl --bs=64K --count=5000000 \
+    --vector=readSpeed --dir=/mnt/disk1 \
+    --logfile=./diskbench-read.log
 
 # Remove the file created
 #rm /mnt/disk1/*.dat 
