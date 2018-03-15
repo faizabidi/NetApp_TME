0a174bc61a37357d6c411be812f1f923427256c5
faizabidi <fabidi89@vt.edu>
2018-03-14 21:35:17 -0400

    Run the diskbench script read and write tests.

---
+++ cloudera/disk-throughput.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Pull the latest changes into the repo
+cd /Users/faiz/git/NetApp_TME && git pull && cd cloudera
+
+# Make the diskbench file executable
+chmod +x diskbench.pl
+
+# Run write test
+/Users/faiz/git/NetApp_TME/cloudera/diskbench.pl --bs=64K --count=5000000 \
+    --vector=writeSpeed --dir=/mnt/disk1/ --logfile=./diskbench-write.log
+
+sleep 30
+
+# Run read test
+/Users/faiz/git/NetApp_TME/cloudera/diskbench.pl --bs=64K --count=5000000 \
+    --vector=readSpeed --dir=/mnt/disk1/ --logfile=./diskbench-read.log
+
+
+# Remove the file created
+rm /mnt/disk1/*.dat
+
+echo "Done. Check the log file for details."
