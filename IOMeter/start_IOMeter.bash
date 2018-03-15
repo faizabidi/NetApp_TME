673adc1b838726cf71ef0c5efd77d3169b61aff1
faizabidi <fabidi89@vt.edu>
2018-03-15 15:31:08 -0400

    Stopped working for whatever reason. Made some changes to make it work (nohup, removed sudo, etc.

--- IOMeter/start_IOMeter.bash
+++ IOMeter/start_IOMeter.bash
@@ -5,17 +5,16 @@ set -ex
 KILL_DYNAMO="ps -ef | grep dynamo | grep -v 'faiz89' | awk '{print \$2}' | xargs -r sudo kill -9"
 
 for i in 136 138 140 142 144 146 163 165; do
-    START_DYNAMO="sudo ./dynamo -i worker-138 -m worker-$i > /tmp/mylogfile 2>&1 \&"
-    ssh -t $i << EOF
+    START_DYNAMO="nohup ./dynamo -i workr-138 -m workr-$i > /tmp/mylogfile 2>&1 \&"
+    ssh $i << EOF
     # Kill any dynamo process already running
     eval $KILL_DYNAMO
     # Now start a fresh dynamo process
     if eval $START_DYNAMO; then
-        echo "Dynamo successfully started on"
-        hostname
+        echo $START_DYNAMO
+        echo "Dynamo successfully started on workr-$i."
     else
-        echo "Problem with"
-        hostname
+        echo "Problem with workr-$i."
     fi
 EOF
 done
