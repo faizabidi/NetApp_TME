5feb4d52c9a329d4b18c34aeb638820f7e53283a
faizabidi <fabidi89@vt.edu>
2018-03-19 11:25:44 -0400

    Add --filelist argument to read and also remove old log files.

--- cloudera/disk-throughput.bash
+++ cloudera/disk-throughput.bash
@@ -19,17 +19,20 @@ chmod +x diskbench.pl
 # Remove the flist.txt file since that will contain old .dat file names
 rm /var/tmp/flist.txt 
 
+# Also remove the old log file
+rm /home/faiz89/git/NetApp_TME/cloudera/diskbench-*-$LOG_FILE_NAME.log
+
 # Run write test
 /home/faiz89/git/NetApp_TME/cloudera/diskbench.pl --bs=64K --count=5000000 \
     --vector=writeSpeed --dir=/mnt/disk1 \
-    --logfile=./diskbench-write-$1.log 
+    --logfile=./diskbench-write-$LOG_FILE_NAME.log 
 
 sleep 30
 
 # Run read test
 /home/faiz89/git/NetApp_TME/cloudera/diskbench.pl --bs=64K --count=5000000 \
-    --vector=readSpeed --dir=/mnt/disk1 \
-    --logfile=./diskbench-read-$1.log
+    --vector=readSpeed --dir=/mnt/disk1 --filelist=/var/tmp/flist.txt \
+    --logfile=./diskbench-read-$LOG_FILE_NAME.log
 
 # Remove the file created
 rm /mnt/disk1/*.dat 
