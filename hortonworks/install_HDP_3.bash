b5dbd76d1e355f61ec40d2aefdcc8ab6a36a10f9
faizabidi <fabidi89@vt.edu>
2018-09-26 19:53:35 -0400

    Install HDP3.

---
+++ hortonworks/install_HDP_3.bash
@@ -0,0 +1,10 @@
+#/bin/bash
+
+sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.6.1.5/ambari.repo -O /etc/yum.repos.d/ambari.repo
+
+sudo yum install ambari-server
+
+sudo ambari-server setup
+
+sudo ambari-server start
+
