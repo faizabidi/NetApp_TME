2d1b20220b42034c39f15db745ce6f6aecf3ee89
faizabidi <fabidi89@vt.edu>
2018-09-26 19:54:39 -0400

    Pass -y option to yum install.

--- hortonworks/install_HDP_3.bash
+++ hortonworks/install_HDP_3.bash
@@ -2,7 +2,7 @@
 
 sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.6.1.5/ambari.repo -O /etc/yum.repos.d/ambari.repo
 
-sudo yum install ambari-server
+sudo yum install ambari-server -y
 
 sudo ambari-server setup
 
