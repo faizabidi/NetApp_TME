4763ca7efd2d7134b710389253b50fa57c4821b3
faizabidi <fabidi89@vt.edu>
2018-10-01 17:09:35 -0400

    Modified paths.

--- tera_n_dfsio_das.bash
+++ tera_n_dfsio_das.bash
@@ -3,71 +3,61 @@
 # Teragen
 # RUN 1
 # Remove the old data first
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input
+hadoop fs -rm -r -skipTrash /terasort-input
 # Run the job
-hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
-    teragen -D mapreduce.job.maps=152 -D mapreduce.job.reduces=152 10995116277 \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input
+hadoop jar \
+    /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+    teragen -D mapreduce.job.maps=152 1099511627 /terasort-input
 sleep 1m
 # RUN 2
 # Remove the old data first
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input
+hadoop fs -rm -r -skipTrash /terasort-input
 # Run the job
-hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
-    teragen -D mapreduce.job.maps=152 -D mapreduce.job.reduces=152 10995116277 \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input
+hadoop jar \
+    /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+    teragen -D mapreduce.job.maps=152 1099511627 /terasort-input
 sleep 1m
 # RUN 3
 # Remove the old data first
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input
+hadoop fs -rm -r -skipTrash /terasort-input
 # Run the job
-hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
-    teragen -D mapreduce.job.maps=152 -D mapreduce.job.reduces=152 10995116277 \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input
+hadoop jar \
+    /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+    teragen -D mapreduce.job.maps=152 1099511627 /terasort-input
 sleep 1m
 #############################################################################################
 # Terasort
 # RUN 1
 # Remove the old output
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-output
+hadoop fs -rm -r -skipTrash /terasort-output
 # Run the job
-hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
-    terasort -D mapreduce.job.reduces=152 \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-output
+hadoop jar \
+    /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+    terasort -D mapreduce.job.reduces=152 /terasort-input /terasort-output
 sleep 1m
 # RUN 2
 # Remove the old output
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-output
+hadoop fs -rm -r -skipTrash /terasort-output
 # Run the job
-hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
-    terasort -D mapreduce.job.reduces=152 \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-output
+hadoop jar \
+    /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+    terasort -D mapreduce.job.reduces=152 /terasort-input /terasort-output
 sleep 1m
 # RUN 3
 # Remove the old output
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-output
+hadoop fs -rm -r -skipTrash /terasort-output
 # Run the job
-hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
-    terasort -D mapreduce.job.reduces=152 \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-output
+hadoop jar \
+    /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
+    terasort -D mapreduce.job.reduces=152 /terasort-input /terasort-output
 sleep 1m
 # Finally remove teragen and terasort input
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-input
-hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_vol1/HiBench/Dfsioe/terasort-output
+hadoop fs -rm -r -skipTrash /terasort-input
+hadoop fs -rm -r -skipTrash /terasort-output
 #############################################################################################
 # DFSIO WRITE
 # RUN 1
+: << 'START'
 hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     TestDFSIO -write -nrFiles 64 -fileSize 4096
 sleep 1m
@@ -92,5 +82,6 @@ sleep 1m
 # RUN 3
 hadoop jar /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     TestDFSIO -read -nrFiles 64 -fileSize 4096
+START
 
 echo "All testing done. Please check the job history server."
