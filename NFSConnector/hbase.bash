a3e811849a0f2e5d21bbebb34b4e2442191c8c89
faizabidi <fabidi89@vt.edu>
2018-10-09 16:56:43 -0400

    Added some parameters and run each test twice.

--- NFSConnector/hbase.bash
+++ NFSConnector/hbase.bash
@@ -4,37 +4,77 @@ CLIENTS=$1
 
 if [ $# -ne 1 ]; then
     echo "Please provide the number of clients to be used to run this script."
-    echo "Example: ./hbase.bash 20"
+    echo "Example: ./hbase.bash 300"
     exit 1
 fi
 
+#####################################
 # Random write
+# Run 1
 hbase pe \
     --presplit=4 \
     --table=faiz_test \
     --writeToWAL=False \
+    --rows=2048576 \
+    --size=2 \
     randomWrite $CLIENTS
 sleep 2m
-
+# Run 2
+hbase pe \
+    --presplit=4 \
+    --table=faiz_test \
+    --writeToWAL=False \
+    --rows=2048576 \
+    --size=2 \
+    randomWrite $CLIENTS
+sleep 2m
+#####################################
 # Random read
+# Run 1
 hbase pe --presplit=4 \
     --table=faiz_test \
     --writeToWAL=False \
+    --size=2048576 \
     randomRead $CLIENTS
 sleep 2m
-
-# Sequential write
+# Run 2
 hbase pe --presplit=4 \
     --table=faiz_test \
     --writeToWAL=False \
+    --size=2048576 \
+    randomRead $CLIENTS
+sleep 2m
+#####################################
+# Sequential write
+# Run 1
+hbase pe --presplit=4 \
+    --table=faiz_test \
+    --writeToWAL=False \
+    --rows=2048576 \
+    --size=2 \
     sequentialWrite $CLIENTS
 sleep 2m
-
-# Sequential read
+# Run 2
 hbase pe --presplit=4 \
     --table=faiz_test \
     --writeToWAL=False \
+    --rows=2048576 \
+    --size=2 \
+    sequentialWrite $CLIENTS
+sleep 2m
+#####################################
+# Sequential read
+# Run 1
+hbase pe --presplit=4 \
+    --table=faiz_test \
+    --writeToWAL=False \
+    --size=2048576 \
     sequentialRead $CLIENTS
-
+# Run 2
+hbase pe --presplit=4 \
+    --table=faiz_test \
+    --writeToWAL=False \
+    --size=2048576 \
+    sequentialRead $CLIENTS
+#####################################
 echo "All testing done."
-
