50bb21d48b5bcd1c682f9d45a397e18858b32975
faizabidi <fabidi89@vt.edu>
2018-10-21 15:34:00 -0400

    Changed to flexvols.

--- tera_n_dfsio_nfs.bash
+++ tera_n_dfsio_nfs.bash
@@ -4,73 +4,73 @@
 # RUN 1
 # Remove the old data first
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input
 # Run the job
 hadoop jar \
     /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     teragen -D mapreduce.job.maps=152 5368709120 \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input
 sleep 1m
 # RUN 2
 # Remove the old data first
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input
 # Run the job
 hadoop jar \
     /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     teragen -D mapreduce.job.maps=152 5368709120 \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input
 sleep 1m
 # RUN 3
 # Remove the old data first
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input
 # Run the job
 hadoop jar \
     /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     teragen -D mapreduce.job.maps=152 5368709120 \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input
 sleep 1m
 #############################################################################################
 # Terasort
 # RUN 1
 # Remove the old output
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-output
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-output
 # Run the job
 hadoop jar \
     /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     terasort -D mapreduce.job.reduces=152 \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-output
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input \
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-output
 sleep 1m
 # RUN 2
 # Remove the old output
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/HiBench/Dfsioe/terasort-output
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/HiBench/Dfsioe/terasort-output
 # Run the job
 hadoop jar \
     /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     terasort -D mapreduce.job.reduces=152 \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-output
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input \
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-output
 sleep 1m
 # RUN 3
 # Remove the old output
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-output
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-output
 # Run the job
 hadoop jar \
     /usr/hdp/3.0.1.0-187/hadoop-mapreduce/hadoop-mapreduce-examples.jar \
     terasort -D mapreduce.job.reduces=152 \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-output
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input \
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-output
 sleep 1m
 # Finally remove teragen and terasort input
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-input
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-input
 hadoop fs -rm -r -skipTrash \
-    nfs://10.63.150.118:2049/faiz_nfs_flex_group/terasort-output
+    nfs://10.63.150.118:2049/faiz_nfs_flexvol/terasort-output
 #############################################################################################
 : << 'START'
 # DFSIO WRITE
