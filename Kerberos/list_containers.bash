a4eea255a9adab5a28fc24c482e29156f5324f1e
Faiz Abidi <fabidi89@vt.edu>
2019-05-10 23:14:54 +0000

    Extract container logs.

---
+++ Kerberos/list_containers.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+JOBID=$1
+if [ $# != 1 ]; then
+	echo "Please enter the name of the jobId. You can get it from the history server GUI."
+	exit 1
+fi
+
+echo "Job id is " $JOBID
+
+extractNumberFromJobId () {
+	APPLICATIONID="application_"$(echo $JOBID | cut -c5-)
+	echo "Application id is" $APPLICATIONID
+}
+
+extractAppAttempt () {
+	APPATTEMPT="$(yarn applicationattempt -list application_1557511521030_0017 | awk '{print $1}' | grep appattempt)"
+	echo "Appattempt id is" $APPATTEMPT
+}
+
+containerList () {
+	ALLCONTAINERS="$(yarn container -list appattempt_1557511521030_0017_000001 | awk '{print $1}' | grep container_)"
+	echo "All the containers in the $JOBID are:"
+	for container in "${ALLCONTAINERS[@]}"; do
+		echo "$container"
+	done
+}
+
+extractNumberFromJobId
+extractAppAttempt
+containerList
+
+echo "To get container logs, just do 'yarn logs -applicationId application_id -containerId container_id'"
+
